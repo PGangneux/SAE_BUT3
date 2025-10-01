cc0314923b123afa786ac9f84f966bd8982993a7
glados-creator <79933806+glados-creator@users.noreply.github.com>
2025-10-01 17:24:11 +0200

    missing bootstrap

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,6 +12,7 @@ export default {
 </script>
 
 <template>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <p>
         <strong>DEBUG Current route path:</strong> {{ $route.fullPath }}
     </p>
