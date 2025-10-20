f3d6dcc38d9553df31264b1fe035a1d5b18da0e6
renaudin-clement <prof.renaudin.clement@gmail.com>
2025-10-20 11:32:42 +0200

    renomage

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import page_acceuil from "./views/acceuil.vue";
+import page_acceuil from "./views/Acceuil.vue";
+import page_admin from "./views/Admin.vue";
 
 
 const routes = [
     { path : "/", component: page_acceuil},
+    { path : "/admin", component: page_admin},
 ]
 
 const router = createRouter({
--- frontend/src/views/Admin.vue
+++ frontend/src/views/Admin.vue
@@ -1,15 +1,17 @@
-<script setup>
-import { ref } from 'vue'
+<script>
+export default {
+    name: "page_admin",
+    components: {
+    },
+};
 
-defineProps({
-  msg: String,
-})
+
 
 const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1> Admin </h1>
 
 
   <nav>
