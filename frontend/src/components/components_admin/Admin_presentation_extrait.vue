4b90f65ad4b9ad5172e9cf3cf2a4de129b75a5e2
renaudin-clement <prof.renaudin.clement@gmail.com>
2025-10-20 15:22:38 +0200

    composant extrait en cours

---
+++ frontend/src/components/components_admin/Admin_presentation_extrait.vue
@@ -0,0 +1,100 @@
+<script>
+
+export default {
+    name: "comp_admin_presentation_extrait",
+    components: {
+    },
+};
+</script>
+
+<template>
+    <div class="container bg-secondary">
+        <div class="row base">
+            <div class="col-xl vigniette ">
+                <h1 class="row decallage_droite">Question Extrait</h1>
+
+                <div class="row text-center bg-light decallage_droite ">
+                    <p> description </p>
+                </div>
+
+                <div class="row decallage_droite">
+                    <ul>
+
+                        <li class="row vigniette">
+                            <img src="../../assets/imgs/date.svg" alt="date logo " class="col" height="32" width="32">
+                            <p class="col" >date publication</p>
+                        </li>
+
+                        <li class="row vigniette">
+                            <img src="../../assets/imgs/tags.svg" alt="tags logo " class="col" height="32" width="32">
+                            <p class="col">tags</p>
+                            <!-- faire get des tags-->
+                        </li>
+
+
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-sm reduction_image">
+                <img src="../../assets/imgs/width550.png" class="video" alt="Video logo" height="150" width="150">
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+
+
+</template>
+
+<style scoped>
+
+.bt{
+    color:white;
+    background-color:#26503C;
+    border-radius: 19%;
+    
+}
+
+ul ,base{
+  display: flex;
+  flex-wrap: nowrap;
+  list-style-type: none;
+  flex-grow: 1;
+
+  
+}
+
+.vigniette{
+    flex-grow: 1;
+}
+
+.decallage_droite{
+    margin-left: 10px;
+}
+
+.container {
+     border-radius: 2em;
+}
+
+.video{
+    border: solid 10px;
+    border-radius: 2em;
+}
+
+.headeradmin{
+    
+    background-color:#2C2D32;
+    margin: 10px 10px 10px 10px; 
+}
+
+.reduction_image{
+    flex-grow: 0;
+    padding: 10px;
+    padding-left: 50px;
+}
+
+</style>
