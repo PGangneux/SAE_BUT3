4fd7869f9208a1fa95245a8ce4aee288141f3611
glados_creator <79933806+glados-creator@users.noreply.github.com>
2025-10-20 16:13:09 +0200

    fix css + search bar proto

--- frontend/src/components/searchbar.vue
+++ frontend/src/components/searchbar.vue
@@ -1,9 +1,23 @@
 <script>
 export default {
     name: "comp_searchbar",
+    data() {
+        var tags = ["pop"];
+        return {
+            tags: tags,
+        };
+    },
+    emits: [
+        "search",
+    ],
 };
 </script>
 
 <template>
-    <p>search bar here</p>
+    <div>
+        <input type="text" placeholder="Search..." />
+    </div>
+    <button @click="$emit('search')"></button>
+    <img src="" alt="">
+
 </template>
