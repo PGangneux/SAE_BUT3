056c323b400407120f0235dc9e86b0f85a750ab6
GangneuxPierre <pierre.gangneux@etu.univ-orleans.fr>
2025-10-20 11:45:20 +0200

    ajout iframe

---
+++ frontend/src/components/IframeLectureVideo.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      required: true
+    },
+  },
+};
+</script>
+
+
+<template>
+  <iframe 
+    :src="url" 
+    title="YouTube video player" 
+    frameborder="0" 
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+    referrerpolicy="strict-origin-when-cross-origin" 
+    allowfullscreen>
+  </iframe>
+</template>
+
+
---
+++ frontend/src/components/LecteurVideo.vue
@@ -0,0 +1,33 @@
+<script>
+import IframeLectureVideo from './IframeLectureVideo.vue';
+import ParametresVideo from "./ParametresVideo.vue";
+import PictureInPicture from "./PictureInPicture.vue";
+import BarListeVideo from "./BarListeVideo.vue";
+
+export default {
+  components: {
+    IframeLectureVideo,
+    ParametresVideo,
+    PictureInPicture,
+    BarListeVideo
+  }
+};
+
+</script>
+
+<template>
+  <main>
+    <IframeLectureVideo url="https://www.youtube.com/embed/10MZrDXjby8" />
+    <div>
+      <h2>Title</h2>
+      <ParametresVideo />
+      <PictureInPicture />
+      <div>
+        <p>Video description</p>
+      </div>
+    </div>
+  </main>
+  <aside>
+    <BarListeVideo />
+  </aside>
+</template>
