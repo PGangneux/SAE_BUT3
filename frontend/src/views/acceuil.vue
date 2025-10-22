299db7f95414b8b4dd3583f2e0fcd5636604add3
GangneuxPierre <pierre.gangneux@etu.univ-orleans.fr>
2025-10-22 11:19:23 +0200

    pip peut etre desactivé

--- frontend/src/views/acceuil.vue
+++ frontend/src/views/acceuil.vue
@@ -13,6 +13,19 @@ export default {
   },
   setup() {
     return { videoStore };
+  },
+
+  methods : {
+    picture_in_picture() {
+      console.log("→ Désactivation du Picture in Picture");
+      videoStore.url = this.url;
+      videoStore.lecteur = this.lecteur;
+      console.log("le lecteur: "+this.lecteur)
+      videoStore.isPictureInPicture = false;
+
+      // Rediriger vers la page d’accueil
+      this.$router.push("/lecteur_video");
+    },
   }
 };
 </script>
@@ -28,6 +41,7 @@ export default {
         class="pip-video"
     >
         <iframe_lecture_video :url="videoStore.url" />
+        <img src="/imgs/affichage_lecteur_réduit_2.svg" alt="picture in picture" @click="picture_in_picture">
     </div>
 </template>
 
@@ -36,12 +50,23 @@ export default {
   position: fixed;
   bottom: 1rem;
   right: 1rem;
-  width: 300px;
-  height: 170px;
-  border: 2px solid var(--vert-neon);
-  border-radius: 10px;
+  width: 30%;
+  height: 30%;
+
   overflow: hidden;
   z-index: 9999;
-  background-color: black;
+
 }
+
+/* L'image est positionnée par rapport au conteneur pip-video */
+.pip-video img {
+  position: absolute; /* position par rapport au parent */
+  top: 0.5rem;        /* un petit offset depuis le haut */
+  left: 0.5rem;       /* un petit offset depuis la gauche */
+  width: 8%;        /* mieux qu'un pourcentage pour un bouton */
+  z-index: 10000;     /* toujours au-dessus du contenu */
+  cursor: pointer;
+
+}
+
 </style>
