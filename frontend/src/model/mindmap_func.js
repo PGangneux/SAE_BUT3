c0b0f5790c2877be9039f7f2b967f7b6f952310f
glados-creator <79933806+glados-creator@users.noreply.github.com>
2025-11-05 17:30:34 +0100

    handle zoom + fix bug zoom + move with click

--- frontend/src/model/mindmap_func.js
+++ frontend/src/model/mindmap_func.js
@@ -41,21 +41,23 @@ class mmLinkage {
         this.thickness = thickness;
     }
 
-    getlength(scale) {
-        return Math.sqrt(Math.pow((this.endnode.x - this.startnode.x + 100 ) * scale, 2) + Math.pow((this.endnode.y - this.startnode.y +100 ) * scale, 2));
-    }
-
-    getangle() {
-        return Math.atan2(this.endnode.y - this.startnode.y, this.endnode.x - this.startnode.x) * 180 / Math.PI;
-    }
 
     getStyle(scale, baseOffsetX, baseOffsetY) {
+        const scaledStartX = this.startnode.x * scale;
+        const scaledStartY = this.startnode.y * scale;
+        const scaledEndX = this.endnode.x * scale;
+        const scaledEndY = this.endnode.y * scale;
+
+        const length = Math.sqrt(Math.pow(scaledEndX - scaledStartX, 2) + Math.pow(scaledEndY - scaledStartY, 2));
+        const angle = Math.atan2(scaledEndY - scaledStartY, scaledEndX - scaledStartX) * 180 / Math.PI;
+
         return {
             'height': (this.thickness * scale) + 'px',
-            'width': (this.getlength(scale)) + 'px',
-            'left': (this.startnode.x + baseOffsetX + 25) + 'px',
-            'top': (this.startnode.y + baseOffsetY + 25) + 'px',
-            'transform': `rotate(${this.getangle(scale)}deg)`,
+            'width': length + 'px',
+            'left': (scaledStartX + baseOffsetX + 50 * scale) + 'px', // 50 * scale to center (half of node size)
+            'top': (scaledStartY + baseOffsetY + 50 * scale) + 'px', // 50 * scale to center
+            'transform': `rotate(${angle}deg)`,
+            'transform-origin': '0 50%',
         };
     }
 }
@@ -76,13 +78,15 @@ class mmNode {
         this.uuid = uuid;
     }
 
-    getStyle(scale, baseOffsetX , baseOffsetY ) {
-        const size = 100 * scale; // Base size 100px multiplied by scale
+    getStyle(scale, baseOffsetX, baseOffsetY) {
+        const size = 100 * scale;
+        const scaledX = this.x * scale;
+        const scaledY = this.y * scale;
 
         return {
             'background-color': LegendColorMap[this.category.name] || '#000000',
-            'left': (this.x + baseOffsetX) + 'px',
-            'top': (this.y + baseOffsetY) + 'px',
+            'left': (scaledX + baseOffsetX) + 'px',
+            'top': (scaledY + baseOffsetY) + 'px',
             'width': size + 'px',
             'height': size + 'px',
             'font-size': (16 * scale) + 'px',
@@ -91,54 +95,60 @@ class mmNode {
     }
 }
 
-function set_children(obj,root,origin_angle){
+function set_children(vueobj, root, origin_angle) {
     // failsafe , si pas enfant
     if (!root.childrens.length) return;
     // distance entre root et enfant ;
     // on a un cercle de 360° , et on doit divisier ca par le nombre d'enfants (moins le trait d'origine) 
-    let nb_child = root.childrens.length + (!! origin_angle ? 2 : 0);
+    let nb_child = root.childrens.length + (origin_angle ? 2 : 0);
     let angle_per_child = (360 / nb_child);
     // so the distance is inversly proportional to the number of angle_per_child
-    let distance = (1/angle_per_child) * 100 + 30;
+    let distance = (1 / angle_per_child) * 300 + 500;
     // we iterate over an angle
     let current_angle = origin_angle || 0;
+    let thickness_base = (vueobj.chemin.length + 1) * 3;
+    let base_rootx = root.x - 25 * vueobj.scale;
+    let base_rooty = root.y - 25 * vueobj.scale;
     for (let index = 0; index < root.childrens.length; index++) {
         const child = root.childrens[index].childnode;
-        child.x = root.x + Math.cos(current_angle) * distance;
-        child.y = root.y + Math.sign(current_angle) * distance;
+        const angleRad = current_angle * Math.PI / 180;
+
+        child.x = base_rootx + Math.cos(angleRad) * distance;
+        child.y = base_rooty + Math.sin(angleRad) * distance;
         root.childrens[index].angle = current_angle;
         // create link
-        obj.linkages.push(new mmLinkage(root,child,(1/obj.chemin.length)*root.depth*2));
+        // console.log("thicness",vueobj.chemin.length,root.depth,thickness_base,thickness_base * (1 / root.depth));
+        vueobj.linkages.push(new mmLinkage(root, child, thickness_base * (1 / root.depth)));
         // advance the angle
-        current_angle += angle_per_child ;
+        current_angle += angle_per_child;
     }
 }
 
-export function mmget(obj){
+export function mmget(vueobj) {
     // reset
-    obj.nodes = [];
-    obj.linkages = [];
+    vueobj.nodes = [];
+    vueobj.linkages = [];
     // create root
-    let root = new mmNode(0,0,0,mmRoot,null);
-    obj.nodes.push(root);
+    let root = new mmNode(0, 0, 1, mmRoot, null);
+    vueobj.nodes.push(root);
     // put defaults
     for (const cat of categorys) {
         // so for each cat in the default categorys
         // create a "root" category buble
-        let tmp_child = new mmNode(0,0,1,cat,null);
+        let tmp_child = new mmNode(0, 0, 2, cat, null);
         // render it
-        obj.nodes.push(tmp_child);
+        vueobj.nodes.push(tmp_child);
         // they are children of the white root node
         root.childrens.push({
-                childnode :tmp_child,
-                angle : null
-            });
+            childnode: tmp_child,
+            angle: null
+        });
     }
     // put default cercle position + links
-    set_children(obj,root,null);
+    set_children(vueobj, root, null);
     let current_node = root;
-    for (const child of obj.chemin) {
+    for (const child of vueobj.chemin) {
         // let next = current_node.childrens. [obj => obj.category == child] ;
     }
-    console.log(root);
+    // console.log(root);
 }
