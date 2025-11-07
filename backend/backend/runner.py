caf49b291ea1b2bb07b369cd13dc32e9ee66505f
baptiste-mignan <baptiste.mignan@gmail.com>
2025-11-07 09:43:26 +0100

    hot fix runner tests

--- backend/backend/runner.py
+++ backend/backend/runner.py
@@ -1,6 +1,7 @@
 from django.test.runner import DiscoverRunner
 from neomodel import db, config
-from API.management.commands.install_labels import Command
+from API.management.commands.install_labels import Command as install_labels
+from API.management.commands.basic_load_bd import Command as basic_load_bd
 
 TEST_BOLT_URL = "bolt://neo4j:testtest@localhost:17687"
 
@@ -9,7 +10,8 @@ class CustomTestRunner(DiscoverRunner):
         super().setup_test_environment(**kwargs)
         print("=== Initialisation Neo4j avant tous les tests ===")
         config.DATABASE_URL = TEST_BOLT_URL
-        Command().handle()
+        install_labels().handle()
+        basic_load_bd().handle()
         db.cypher_query("MATCH (n) DETACH DELETE n")
 
     def teardown_test_environment(self, **kwargs):
