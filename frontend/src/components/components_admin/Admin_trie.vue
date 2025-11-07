2c795557d504a959a5b48525af5ce547dced6219
renaudin-clement <prof.renaudin.clement@gmail.com>
2025-11-07 15:52:53 +0100

    retenir

--- frontend/src/components/components_admin/Admin_trie.vue
+++ frontend/src/components/components_admin/Admin_trie.vue
@@ -75,7 +75,7 @@ export default {
 <style scoped>
 .scroller {
     width: 300px;
-    height: 100vh;
+    height: 32vh;
     overflow-y: scroll;
     scrollbar-color: var(---blanc) #A6A6A6;
     scrollbar-width: thin;
@@ -90,7 +90,6 @@ justify-content: center
 
 .tagsfully{
     width: 100%;
-    height: 100%;
     flex-grow: 1;
 }
 
