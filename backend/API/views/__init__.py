5d0e88321ff328ab5075ebf8b161339d6ed27710
baptiste-mignan <baptiste.mignan@gmail.com>
2025-11-14 13:54:53 +0100

    mise en place classe viewsets Base et subBase

--- backend/API/views/__init__.py
+++ backend/API/views/__init__.py
@@ -1,3 +1,68 @@
+from django.http import HttpRequest
+from django.db.models.query import QuerySet
+from rest_framework import viewsets, serializers
+from neomodel.sync_.match import NodeSet
+from neomodel import StructuredNode
+
+class Base(viewsets.ModelViewSet):
+    """
+    Classe de base pour les ModelViewSets classiques
+    """
+    lookup_field = 'uuid'
+
+    def __init__(self, serializer_class: serializers.Serializer, model_class: StructuredNode, search_field: str = None, **kwargs):
+        super().__init__(**kwargs)
+        self.serializer_class: serializers.Serializer = serializer_class
+        self.model_class: StructuredNode = model_class
+        self.search_field: str = search_field
+
+    def get_nodeset(self) -> NodeSet:
+        # Voir pour les sous endpoints s'il n'est pas possible de build le NodeSet à partir d'une liste de Node
+        return self.model_class.nodes
+
+    def get_queryset(self) -> QuerySet:
+        queryset: NodeSet = self.get_nodeset()
+        request: HttpRequest = self.request
+        if self.search_field:
+            search: str = request.GET.get('search', '').strip()
+            for term in search.split(','):
+                if term:
+                    # Le search field n'étant pas identique,
+                    # il est nécessaire de filtrer ainsi
+                    queryset = queryset.filter(
+                        **{f'{self.search_field}__icontains': term}
+                    )
+        return queryset.all()
+    
+    def get_object(self) -> StructuredNode:
+        try:
+            return self.get_nodeset().get(uuid=self.kwargs[self.lookup_field])
+        except DoesNotExist:
+            raise NotFound(self.model_class)
+
+
+class subBase(Base):
+    """
+    
+    """
+
+    def __init__(self, serializer_class, model_class, router_lookup_field, router_model_class, relationship, search_field = None, **kwargs):
+        super().__init__(serializer_class, model_class, search_field, **kwargs)
+        self.router_lookup_field = router_lookup_field
+        self.router_model_class = router_model_class
+        self.relationship = relationship
+
+    def get_nodeset(self) -> NodeSet:
+        query = "MATCH (q:" + self.model_class.__name__ + ")-[:" + self.relationship + "]->(t:"+ self.router_model_class.__name__ + " {uuid: $uuid}) RETURN q.uuid"
+        nodeset = super().get_nodeset().filter(uuid__in=[
+                uuid[0] for uuid in db.cypher_query(
+                    query, {'uuid': self.kwargs[self.router_lookup_field]}
+                )[0]
+            ]
+        )
+        return nodeset
+
+
 from .theme import *
 from .question import *
 from .extrait import *
--- backend/API/views/artiste.py
+++ backend/API/views/artiste.py
@@ -1,39 +1,19 @@
 from rest_framework import viewsets
 from neomodel.exceptions import DoesNotExist
 from neomodel import db
+from ..views import Base, subBase
 from ..errors import NotFound
-from ..models import Artiste
+from ..models import Artiste, Nation
 from ..serializers import ArtisteSerializer
 
 
-class ArtisteViewSet(viewsets.ModelViewSet):
+class ArtisteViewSet(Base):
     """
     Renvoie les artistes
     """
-    serializer_class = ArtisteSerializer
-    lookup_field = 'uuid'
-
-    def get_queryset(self):
-        """
-        Récupération du QuerySet
-        """
-        artistes = Artiste.nodes
-        search = self.request.query_params.get('search', '').strip()
-        if not search:
-            return artistes.all()
-        for term in search.split():
-            if term:
-                artistes = artistes.filter(name__icontains=term)
-        return artistes.all()
     
-    def get_object(self):
-        """
-        Récupération de l'Objet
-        """
-        try:
-            return Artiste.nodes.get(uuid=self.kwargs[self.lookup_field])
-        except DoesNotExist:
-            raise NotFound(Artiste)
+    def __init__(self, **kwargs):
+        super().__init__(ArtisteSerializer, Artiste, 'name', **kwargs)
 
 
 class StyleMusicalArtisteViewSet(viewsets.ModelViewSet):
@@ -63,28 +43,10 @@ class StyleMusicalArtisteViewSet(viewsets.ModelViewSet):
         return Artiste.inflate(results[0][0])
 
 
-class NationArtisteViewSet(viewsets.ModelViewSet):
+class NationArtisteViewSet(subBase):
     """
     Renvoie les artistes en fonction de leur nationnalite
     """
-    serializer_class = ArtisteSerializer
-    router_lookup_field = 'nation_uuid'
-    lookup_field = 'uuid'
-
-    def get_queryset(self):
-        """
-        Récupération du QuerySet
-        """
-        query = "MATCH (q:Artiste)-[:NATIONALITE]->(t:Nation {uuid: $uuid}) RETURN q"
-        results = db.cypher_query(query, {'uuid': self.kwargs[self.router_lookup_field]})[0]
-        return [Artiste.inflate(row[0]) for row in results]
     
-    def get_object(self):
-        """
-        Récupération de l'Objet
-        """
-        query = "MATCH (q:Artiste {uuid: $uuid})-[:NATIONALITE]->(t:Nation {uuid: $nation}) RETURN q"
-        results = db.cypher_query(query, {'nation': self.kwargs[self.router_lookup_field], 'uuid': self.kwargs[self.lookup_field]})[0]
-        if not results:
-            raise NotFound(Artiste)
-        return Artiste.inflate(results[0][0])
+    def __init__(self, **kwargs):
+        super().__init__(ArtisteSerializer, Artiste, 'nation_uuid', Nation, 'NATIONALITE', 'name', **kwargs)
