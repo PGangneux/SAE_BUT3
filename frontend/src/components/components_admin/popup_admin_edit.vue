dc86df60506407fbd343b6e69632803937adb0a3
renaudin-clement <prof.renaudin.clement@gmail.com>
2025-12-03 18:08:11 +0100

    petit modification de formulation

--- frontend/src/components/components_admin/popup_admin_edit.vue
+++ frontend/src/components/components_admin/popup_admin_edit.vue
@@ -21,7 +21,7 @@ export default {
             tags: {type:Tags},
         };
     },methods: {
-        sendData () {
+        changement_etat_popup () {
             this.$emit('ecoutepopup', !this.popup)
         },
         tags_to_string(tags_array) {
@@ -62,7 +62,7 @@ export default {
 
 
 <template>
-<div class="allmightygris" @click="sendData"></div>
+<div class="allmightygris" @click="changement_etat_popup"></div>
 
 <div class="grisee allmighty trie-tagsfoncer row">
     <div class="col collumpopu ">
@@ -113,7 +113,7 @@ export default {
     </div>
     <div class="col collx">
         <div class="row">
-            <button type="button" class="btn-close btn-close-white" aria-label="Close" @click="sendData"></button>
+            <button type="button" class="btn-close btn-close-white" aria-label="Close" @click="changement_etat_popup"></button>
         </div>
     </div>
 </div>
