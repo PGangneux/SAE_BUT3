a11c976726f674d85536e2def90f9e223ea67d47
baptiste-mignan <baptiste.mignan@gmail.com>
2025-12-18 21:15:35 +0100

    feat(frontend): ajout de Audio dans le frontend

---
+++ frontend/src/model/audio.js
@@ -0,0 +1,33 @@
+import Model from "./model.js";
+import Extrait from "./extrait.js";
+
+export default class Audio extends Model {
+    #name;
+    #extraits;
+
+    constructor({ uuid, name, extraits }) {
+        super(uuid);
+        this.#name = name;
+        this.#extraits = extraits;
+    }
+
+    static get endpoint() { return "audios"; }
+
+    get name() { return this.#name; }
+    set name(value) { this.#name = this.validateString(value, "name"); }
+
+    async extraits(args) { return await this.fetchList(this.#extraits, Extrait, args); }
+
+    fromJSON(json) {
+        super.fromJSON(json);
+        this.#name = json.name;
+        this.#extraits = json.extraits;
+        return this;
+    }
+
+    toJSON(json = {}) {
+        json = super.toJSON(json);
+        if (this.name) json['name'] = this.#name;
+        return json;
+    }
+}
