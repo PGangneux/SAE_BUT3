505e3cb27de8caad52eb0b307fcebe23deb2f6af
GangneuxPierre <pierre.gangneux@etu.univ-orleans.fr>
2025-12-18 09:38:38 +0100

    feature - lecteur video, bar liste video - bouton  plus de video

--- frontend/src/components/lecteur_video/bar_liste_video.vue
+++ frontend/src/components/lecteur_video/bar_liste_video.vue
@@ -12,25 +12,26 @@ import Interview from '../../model/interview';
 export default {
   components: { miniature_video, },
   emits: ["toggle_aside", 'update'],
-  inject : ["extrait_current", "interview_current"],
+  inject: ["extrait_current", "interview_current"],
   props: {
     liste_extraits_current_interview: {
       type: Object,
-      
+
     }
   },
   data() {
     return {
       videos: null,
-      selected : "",
+      selected: "",
       img_close: true,
       extrait: null,
       interview: null,
+      page: 0,
 
     };
   },
 
-  methods : {
+  methods: {
     /**
      * Génère un dictionnaire de poids selon le chemin d'entrée de l'utilisateur.
      * les poids sont plus lourd au debut du chemin.
@@ -38,10 +39,10 @@ export default {
      * @param {Array} chemin - liste représentant le chemin d'entrée de l'utilisateur
      * @return {Dict} Dictionnaire des poids pour les recommandations
     */
-    get_reco_weights(chemin){
+    get_reco_weights(chemin) {
       const weights = {};
       console.log("bar_list_video get_reco_weights", chemin);
-      
+
       chemin.forEach((value, index, array) => {
         console.log("bar_list_video get_reco_weights", value);
         weights[value.constructor.mmch_dbjsclass.name] = array.length - index;
@@ -51,10 +52,14 @@ export default {
 
 
     // À déplacer
-    async current_reco(){
-      this.selected = "reco";
+    async current_reco() {
+      if (this.selected != "reco") {
+        this.page = 0
+        this.selected = "reco";
+        this.videos = []
+      }
       // Feature-flag de l'algorithme de recommandation
-      if (true){
+      if (true) {
         const video = this.extrait ? this.extrait.uuid : this.interview ? this.interview.uuid : null;
         // TODO Nécessite d'enregistrer et modifier les poids à chaque fois
         let weights;
@@ -66,27 +71,29 @@ export default {
         else {
           weights = this.get_reco_weights(videoStore.chemin);
         }
-        this.videos = markRaw(
-          await ClientAPI.post(
-            `${ClientAPI.BASE_URL}api/recommandations`,
-            JSON.stringify({'weights': weights}),
-            ClientAPI.current_user ? true : false,
-            video ? {'video': video, 'size': 10} : null
-          )
-          .then(
-            json => {
-              return json.map(
-                (v) => {
-                  if (v.type == 'Extrait') { return markRaw( new Extrait(v.value) ); }
-                  else { return markRaw( new Interview(v.value) ); }
+        this.videos = this.videos.concat(
+          markRaw(
+            await ClientAPI.post(
+              `${ClientAPI.BASE_URL}api/recommandations`,
+              JSON.stringify({ 'weights': weights }),
+              ClientAPI.current_user ? true : false,
+              video ? { 'video': video, 'size': 10, 'page':this.page } : null
+            )
+              .then(
+                json => {
+                  return json.map(
+                    (v) => {
+                      if (v.type == 'Extrait') { return markRaw(new Extrait(v.value)); }
+                      else { return markRaw(new Interview(v.value)); }
+                    }
+                  );
                 }
-              );
-            }
+              )
           )
         );
       }
       else {
-        this.videos = markRaw(await Extrait.list({size:10})); // récupère les 10 derniers extraits/interviews
+        this.videos = markRaw(await Extrait.list({ size: 10 })); // récupère les 10 derniers extraits/interviews
       }
       console.log('vidéo', this.videos);
     },
@@ -94,14 +101,18 @@ export default {
 
 
 
-    async interview_current_extrait(){
+    async interview_current_extrait() {
       this.selected = "playlists"
       this.videos = markRaw(await this.extrait.interviews())
     },
 
 
-    async extraits_current_question(){
-      this.selected = "questions"
+    async extraits_current_question() {
+      if (this.selected != "questions") {
+        this.page = 0
+        this.selected = "questions"
+        this.videos = []
+      }
       const video = this.extrait ? this.extrait.uuid : this.interview ? this.interview.uuid : null;
       // TODO Nécessite d'enregistrer et modifier les poids à chaque fois
       let weights;
@@ -114,28 +125,34 @@ export default {
         weights = this.get_reco_weights(videoStore.chemin);
       }
       const question = markRaw(await this.extrait.question);
-      this.videos = markRaw(
-        await ClientAPI.post(
-          `${ClientAPI.BASE_URL}api/recommandations`,
-          JSON.stringify({'weights': weights, 'filters': {'Question': question.uuid}}),
-          ClientAPI.current_user ? true : false,
-          video ? {'video': video, 'size': 10} : null
-        )
-        .then(
-          json => {
-            return json.map(
-              (v) => {
-                if (v.type == 'Extrait') { return markRaw( new Extrait(v.value) ); }
-                else { return markRaw( new Interview(v.value) ); }
+      this.videos = this.videos.concat(
+        markRaw(
+          await ClientAPI.post(
+            `${ClientAPI.BASE_URL}api/recommandations`,
+            JSON.stringify({ 'weights': weights, 'filters': { 'Question': question.uuid } }),
+            ClientAPI.current_user ? true : false,
+            video ? { 'video': video, 'size': 10, 'page':this.page } : null
+          )
+            .then(
+              json => {
+                return json.map(
+                  (v) => {
+                    if (v.type == 'Extrait') { return markRaw(new Extrait(v.value)); }
+                    else { return markRaw(new Interview(v.value)); }
+                  }
+                );
               }
-            );
-          }
-        )
-      );
+            )
+          )
+        );
     },
 
     async extraits_interviews_current_artiste() {
-      this.selected = "artiste";
+      if (this.selected != "artiste") {
+        this.page = 0
+        this.selected = "artiste";
+        this.videos = []
+      }
       const video = this.extrait ? this.extrait.uuid : this.interview ? this.interview.uuid : null;
       // TODO Nécessite d'enregistrer et modifier les poids à chaque fois
       let weights;
@@ -148,29 +165,35 @@ export default {
         weights = this.get_reco_weights(videoStore.chemin);
       }
       const artiste = markRaw(await this.extrait.artiste);
-      this.videos = markRaw(
-        await ClientAPI.post(
-          `${ClientAPI.BASE_URL}api/recommandations`,
-          JSON.stringify({'weights': weights, 'filters': {'Artiste': artiste.uuid}}),
-          ClientAPI.current_user ? true : false,
-          video ? {'video': video, 'size': 10} : null
-        )
-        .then(
-          json => {
-            return json.map(
-              (v) => {
-                if (v.type == 'Extrait') { return markRaw( new Extrait(v.value) ); }
-                else { return markRaw( new Interview(v.value) ); }
+      this.videos = this.videos.concat(
+        markRaw(
+          await ClientAPI.post(
+            `${ClientAPI.BASE_URL}api/recommandations`,
+            JSON.stringify({ 'weights': weights, 'filters': { 'Artiste': artiste.uuid } }),
+            ClientAPI.current_user ? true : false,
+            video ? { 'video': video, 'size': 10, 'page':this.page } : null
+          )
+            .then(
+              json => {
+                return json.map(
+                  (v) => {
+                    if (v.type == 'Extrait') { return markRaw(new Extrait(v.value)); }
+                    else { return markRaw(new Interview(v.value)); }
+                  }
+                );
               }
-            );
-          }
-        )
-      );
-      
+            )
+            )
+        );
+
     },
 
-    async extrait_current_theme(){
-      this.selected = "thèmes"
+    async extrait_current_theme() {
+      if (this.selected != "thèmes") {
+        this.page = 0
+        this.selected = "thèmes"
+        this.videos = []
+      }
       const video = this.extrait ? this.extrait.uuid : this.interview ? this.interview.uuid : null;
       // TODO Nécessite d'enregistrer et modifier les poids à chaque fois
       let weights;
@@ -184,38 +207,40 @@ export default {
       }
       const question = markRaw(await this.extrait.question);
       const theme = markRaw(await question.theme);
-      this.videos = markRaw(
-        await ClientAPI.post(
-          `${ClientAPI.BASE_URL}api/recommandations`,
-          JSON.stringify({'weights': weights, 'filters': {'Thème': theme.uuid}}),
-          ClientAPI.current_user ? true : false,
-          video ? {'video': video, 'size': 10} : null
-        )
-        .then(
-          json => {
-            return json.map(
-              (v) => {
-                if (v.type == 'Extrait') { return markRaw( new Extrait(v.value) ); }
-                else { return markRaw( new Interview(v.value) ); }
+      this.videos = this.videos.concat(
+        markRaw(
+          await ClientAPI.post(
+            `${ClientAPI.BASE_URL}api/recommandations`,
+            JSON.stringify({ 'weights': weights, 'filters': { 'Thème': theme.uuid } }),
+            ClientAPI.current_user ? true : false,
+            video ? { 'video': video, 'size': 10, 'page':this.page } : null
+          )
+            .then(
+              json => {
+                return json.map(
+                  (v) => {
+                    if (v.type == 'Extrait') { return markRaw(new Extrait(v.value)); }
+                    else { return markRaw(new Interview(v.value)); }
+                  }
+                );
               }
-            );
-          }
-        )
-      );
+            )
+          )
+        );
     },
-    
+
 
 
 
     async update_liste_video(video) {
-
+      console.log("update liste videp");
       // maj du extrait_current ou interview_current selon le type de video
 
       if (video.extraits) {
         // c'est une interview
         /// console.log(video)
         this.interview_current.set(video);
-        
+
 
         ///console.log((await video.extraits)[0])
         this.extrait_current.set((await video.extraits())[0]);
@@ -227,7 +252,9 @@ export default {
       // recupération des nouveau extrait et interview
       this.extrait = await this.extrait_current.get();
       this.interview = await this.interview_current.get();
-      
+
+
+
       // reset du videoStore
       videoStore.isPlaying = true;
       clearInterval(videoStore.intervalId);
@@ -235,19 +262,30 @@ export default {
       videoStore.currentTime = 0;
 
       this.$emit('update');
-      
+
+      //update bar liste video
+      this.selected = '';
+      console.log("this.current_reco test") 
+      await this.current_reco()
+      console.log("after update video reco", this.videos)
+
       this.$refs.miniature_videos.forEach(child => {
         child.update_miniature();
       });
 
-
       
     },
 
+    async plus_video() {
+      this.page++;
+      switch (this.selected) {
+        case 'reco': await this.current_reco(); break;
+        case 'questions': await this.extraits_current_question(); break;
+        case 'artiste': await this.extraits_interviews_current_artiste(); break;
+        case 'thèmes': await this.extrait_current_theme(); break;
+      }
 
-    
-
-
+    },
 
   },
   async mounted() {
@@ -264,88 +302,100 @@ export default {
 </script>
 
 <template>
-    <header>
-        <nav class="header-nav">
-            <ul class="menu">
-              <li @click="current_reco" :class="{selected: selected === 'reco'}">Recomendation</li>
-              <!--si la video est un extrait-->
-              <li v-if="this.interview === null" @click="extraits_interviews_current_artiste" :class="{selected: selected === 'artiste'}">Artiste</li>
-              <li v-if="this.interview === null" @click="extraits_current_question" :class="{ selected: selected === 'questions' }">Questions</li>
-              <li v-if="this.interview === null" @click="extrait_current_theme" :class="{selected: selected === 'thèmes'}">Thèmes</li>
-              <li v-if="this.interview === null" @click="interview_current_extrait" :class="{selected: selected === 'playlists'}">Playlists</li>
-              
-            </ul>
-            
-            <img v-if="img_close" src="/imgs/close.svg" alt="close" @click="this.$emit('toggle_aside')">
-        </nav>
+  <header>
+    <nav class="header-nav">
+      <ul class="menu">
+        <li @click="current_reco" :class="{ selected: selected === 'reco' }">Recomendation</li>
+        <!--si la video est un extrait-->
+        <li v-if="this.interview === null" @click="extraits_interviews_current_artiste"
+          :class="{ selected: selected === 'artiste' }">Artiste</li>
+        <li v-if="this.interview === null" @click="extraits_current_question"
+          :class="{ selected: selected === 'questions' }">Questions</li>
+        <li v-if="this.interview === null" @click="extrait_current_theme" :class="{ selected: selected === 'thèmes' }">
+          Thèmes</li>
+        <li v-if="this.interview === null" @click="interview_current_extrait"
+          :class="{ selected: selected === 'playlists' }">Playlists</li>
 
-    </header>
-    <main>
-        <div>
+      </ul>
+
+      <img v-if="img_close" src="/imgs/close.svg" alt="close" @click="this.$emit('toggle_aside')">
+    </nav>
+
+  </header>
+  <main>
+    <div>
+      <div>
+        <ul class="liste_video">
+          <li v-for="(video) in videos" :key="video.uuid">
             <div>
-                <ul class="liste_video">
-                    <li v-for="(video) in videos" :key="video.uuid">
-                        <div>
-                              <miniature_video 
-                                ref="miniature_videos" 
-                                @click="update_liste_video(video)"
-                                :video="video" 
-                              />
-                          <div class="video_text">
-                              <h4>{{ video.titre }}</h4>
-                              <p>{{ video.description }}</p>
-                              
-                          </div>
-                        </div>
-                    </li>
-                </ul>
+              <miniature_video ref="miniature_videos" @click="update_liste_video(video)" :video="video" />
+              <div class="video_text">
+                <h4>{{ video.titre }}</h4>
+                <p>{{ video.description }}</p>
+
+              </div>
             </div>
-        </div>
-    </main>
+          </li>
+        </ul>
+        <button @click="plus_video()">plus de video ...</button>
+      </div>
+    </div>
+  </main>
 
 </template>
 
 <style scoped>
-header, main{
-    background-color: var(--gris-foncer);
-    padding: 0 0.5rem;
+header,
+main {
+  background-color: var(--gris-foncer);
+  padding: 0 0.5rem;
 }
 
-header{
-    border-bottom: 1px solid var(--blanc);
-    height: 5%;
-    display: flex;
-    align-items: center;     /* centre verticalement */
+header {
+  border-bottom: 1px solid var(--blanc);
+  height: 5%;
+  display: flex;
+  align-items: center;
+  /* centre verticalement */
 }
 
 main {
-  height: 64%; /* 100-5(header)-30(timecode)-1 */
-  flex-grow: 1; /* permet à main de prendre tout l'espace restant */
-  overflow-y: auto; /* permet le scroll vertical */
+  height: 64%;
+  /* 100-5(header)-30(timecode)-1 */
+  flex-grow: 1;
+  /* permet à main de prendre tout l'espace restant */
+  overflow-y: auto;
+  /* permet le scroll vertical */
 }
+
 .header-nav {
   width: 100%;
   display: flex;
-  justify-content: space-between; /* menu à gauche, bouton X à droite */
-  align-items: center;            /* centre verticalement */
-  
+  justify-content: space-between;
+  /* menu à gauche, bouton X à droite */
+  align-items: center;
+  /* centre verticalement */
+
 }
 
-.header-nav > img {
+.header-nav>img {
   width: 6%;
   height: 6%;
 
 
   cursor: pointer;
-  
+
 }
 
 
 
 .menu {
-  display: flex;       /* aligne les <li> horizontalement */
-  list-style: none;    /* supprime les puces */
-  gap: 1rem;           /* espace entre les items */
+  display: flex;
+  /* aligne les <li> horizontalement */
+  list-style: none;
+  /* supprime les puces */
+  gap: 1rem;
+  /* espace entre les items */
   margin: 0;
   padding: 0;
 }
@@ -388,35 +438,31 @@ main {
   padding: 0;
   cursor: pointer;
   display: flex;
-  gap : 10px;
+  gap: 10px;
   flex-direction: column;
 }
 
-.liste_video li > div {
+.liste_video li>div {
   list-style: none;
-  display: flex;  
+  display: flex;
 }
 
 
 
-.liste_video a{
-    width: 55%;
-    height: 55%;
-    margin-right: 1em;
-    margin-bottom: 2em;
+.liste_video a {
+  width: 55%;
+  height: 55%;
+  margin-right: 1em;
+  margin-bottom: 2em;
 }
 
-.video_text{
+.video_text {
   padding-right: 5%;
   flex-grow: 1;
 }
 
-p, h4{
+p,
+h4 {
   margin: 0;
 }
-
-
-
-
-
 </style>
