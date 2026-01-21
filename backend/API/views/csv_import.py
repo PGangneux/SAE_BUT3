bb4cedcc50c86128dc5ee2dc13c37c016c63425a
GangneuxPierre <pierre.gangneux@etu.univ-orleans.fr>
2026-01-21 15:33:13 +0100

    feature import csv - ajout data une ligne

--- backend/API/views/csv_import.py
+++ backend/API/views/csv_import.py
@@ -6,6 +6,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from ..serializers import ArtisteSerializer, ThemeSerializer, QuestionSerializer, TagSerializer
+
 
 
 class CSVImportView(APIView):
@@ -29,7 +31,17 @@ class CSVImportView(APIView):
                 status=status.HTTP_400_BAD_REQUEST
             )
 
-        # Lecture du fichier (UTF-8 recommandé)
+
+        self.save_data(csv_file)
+
+        return Response(
+            {"message": "lignes importées avec succès"},
+            status=status.HTTP_201_CREATED
+        )
+    
+
+    def save_data(self, csv_file):
+                # Lecture du fichier (UTF-8 recommandé)
         data = csv_file.read().decode("utf-8")
         io_string = io.StringIO(data)
 
@@ -37,7 +49,64 @@ class CSVImportView(APIView):
 
         for row in reader:
             print(row)  # Pour debug
-        return Response(
-            {"message: lignes importées avec succès"},
-            status=status.HTTP_201_CREATED
-        )
+            print(type(row))  # Pour debug
+            self.save_row(row)
+
+
+
+
+
+    def save_row(self, row):
+        serializer_artiste = ArtisteSerializer()
+        artiste  = row["Artiste"].strip() 
+        if artiste != "" and artiste is not None:
+            try:
+                serializer_artiste.create({"name": artiste})
+            except:
+                pass
+
+        serializer_theme = ThemeSerializer()
+        theme  = row["Theme"].strip() 
+        if theme != "" and theme is not None:
+            try:
+                serializer_theme.create({"name": theme})
+            except:
+                pass
+
+        serializer_question = QuestionSerializer()
+        question  = row["Question"].strip()
+        if question != "" and question is not None:
+            try:
+                serializer_question.create({"text": question})
+            except:
+                pass
+        
+        serializer_tag = TagSerializer()
+        tags = [tag.strip() for tag in row["Tags"].split("; ")]
+        for tag in tags:
+            if tag != "" and tag is not None:
+                try:
+                    serializer_tag.create({"name": tag})
+                except:
+                    pass
+
+        
+        
+
+
+
+
+        #serializer_theme = Theme
+
+
+        # return {
+        #     "artiste": row["ARTISTE"].strip(),
+        #     "theme": row["Thème"].strip(),
+        #     "question": row["Question"].strip(),
+        #     "tags": [tag.strip() for tag in row["Tags"].split(",")],
+        #     "date": row["Date"].strip(),
+        #     "evenement": row["Evénement"].strip(),
+        #     "ville": row["Ville"].strip(),
+        #     "youtube": row["YouTube"].strip(),
+        #     "vimeo": row["Vimeo"].strip()
+        # }
