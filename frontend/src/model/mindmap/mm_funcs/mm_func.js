3062ad3557bf6dd570977c759b541431e147c135
glados-creator <79933806+glados-creator@users.noreply.github.com>
2026-01-21 16:38:09 +0000

    mm fix bug + set videostore path correctly

--- frontend/src/model/mindmap/mm_funcs/mm_func.js
+++ frontend/src/model/mindmap/mm_funcs/mm_func.js
@@ -1,5 +1,5 @@
 import { markRaw } from "vue";
-import { mm_createChildNode, set_children_pos } from "./mm_func_node.js"
+import { mm_createChildNode } from "./mm_func_node.js"
 import { mm_find_compare } from "./mm_func_chemin.js";
 import mm_Mindmap from "../mm_mindmap.js";
 import mmch_CheminT from "../mm_chemin_submod/mmch_chemin.js";
@@ -84,7 +84,7 @@ export async function mm_reset_soft(mminfo) {
                 // Create the nodes that are different/new
                 for (let i = sameCount; i < search_cat.length; i++) {
                     const nodeData = search_cat[i];
-                    await mm_createChildNode(mminfo, rootNode, nodeData, true, false);
+                    mm_createChildNode(mminfo, rootNode, nodeData, true, false);
                 }
             }
         } else {
@@ -117,7 +117,6 @@ export async function mm_draw_onecat(mminfo, node, createLink = true, isPreview 
     /// await new Promise(r => setTimeout(r, 1000));
     node.loading = true;
     try {
-
         if (node.ispreview) {
             if (node.depth > mminfo.chemin.length + 4) {
                 // safeguard to avoid expanding too deep previews
@@ -144,12 +143,12 @@ export async function mm_draw_onecat(mminfo, node, createLink = true, isPreview 
                 }
                 /// TMP
                 node.loading = false;
-                for (const child of node.childrens) {
-                    // if that node is parent then only draw content preview
-                    if (child.mmch_obj) {
-                        await mm_draw_onecat(mminfo, child, true, true);
-                    }
-                }
+                // for (const child of node.childrens) {
+                //     // if that node is parent then only draw content preview
+                //     if (child.mmch_obj) {
+                //         await mm_draw_onecat(mminfo, child, true, true);
+                //     }
+                // }
             }
         } else if (node.depth > mminfo.chemin.length + 2) {
             // safeguard to avoid expanding too deep previews
@@ -167,10 +166,10 @@ export async function mm_draw_onecat(mminfo, node, createLink = true, isPreview 
                 mm_createChildNode(mminfo, node, instnode,isPreview);
             }
             /// TMP
-            node.loading = false;
-            for (const child of node.childrens) {
-                await mm_draw_onecat(mminfo, child, true, true);
-            }
+            // node.loading = false;
+            // for (const child of node.childrens) {
+            //     await mm_draw_onecat(mminfo, child, true, true);
+            // }
         } else {
             // ─────────────────────────────
             // 1a. CATEGORY NODE → CONTENT
@@ -185,10 +184,10 @@ export async function mm_draw_onecat(mminfo, node, createLink = true, isPreview 
                 mm_createChildNode(mminfo, node, catnode, createLink,isPreview);
             }
             /// TMP
-            node.loading = false;
-            for (const child of node.childrens) {
-                await mm_draw_onecat(mminfo, child, true, true);
-            }
+            // node.loading = false;
+            // for (const child of node.childrens) {
+            //     await mm_draw_onecat(mminfo, child, true, true);
+            // }
         }
         if (node.depth > mminfo.chemin.length) {
             // ─────────────────────────────
--- frontend/src/model/mindmap/mm_funcs/mm_interface.js
+++ frontend/src/model/mindmap/mm_funcs/mm_interface.js
@@ -5,12 +5,13 @@ import { mm_reset_soft , mm_draw_onecat } from "./mm_func.js";
 
 /**
  *  redraw everynode from root
- * @param {mm_Mindmap} mminfo mm_Mindmap  
+ * @param {mm_Mindmap} mminfo mm_Mindmap 
+ * @returns {Promise<Boolean>} if it's a video or not 
 */
 export async function mm_draw_root(mminfo) {
     let reset_hard = await mm_reset_soft(mminfo);
     let changevideo, changepath = mm_chemin_filter(mminfo);
-    if (changevideo) return;
+    if (changevideo) return true;
     /// console.log("mm_draw_root reset_hard:",reset_hard,"changepath:",changepath);
     if (!reset_hard) {
         for (const child of mminfo.nodes[0].childrens) {
@@ -21,23 +22,33 @@ export async function mm_draw_root(mminfo) {
     for (const cheminpath of mminfo.chemin) {
         await mm_draw_onecat(mminfo, cheminpath);
     }
+    return false;
 }
 
 /**
  *  redraw an update
  * @param {mm_Mindmap} mminfo mm_Mindmap  
+ * @returns {Promise<Boolean>} if it's a video or not 
 */
 export async function mm_draw_update(mminfo) {
     if (mminfo.chemin.length <= 0) return mm_draw_root(mminfo);
     let changevideo, changepath = mm_chemin_filter(mminfo);
-    if (changevideo) return;
+    if (changevideo) return true;
     if (changepath) return mm_draw_root(mminfo);
     await mm_draw_onecat(mminfo, mminfo.chemin[mminfo.chemin.length - 1]);
+    return false;
 }
 
+/**
+ * the main entry interface for Mind Map shenanigans 
+*/
 export async function mm_interface_handleclick(mminfo,node){
     mminfo.chemin.push(node);
-    videoStore.chemin = mminfo.chemin;
     console.log("mm hanldeclick",mminfo.chemin);
-    await mm_draw_update(mminfo);
+    let isvideo = await mm_draw_update(mminfo);
+    if (isvideo){
+        console.log("video store path before",videoStore.chemin);
+        console.log("video store set",mminfo.chemin,mminfo);
+        videoStore.chemin = mminfo.chemin;
+    }
 }
