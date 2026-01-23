927566930386075743edc69800ade1e222605d7c
baptiste-mignan <baptiste.mignan@gmail.com>
2026-01-23 23:31:24 +0100

    style(frontend): checkbox 'rester connecter' -> toggle switch

--- frontend/src/views/connexion.vue
+++ frontend/src/views/connexion.vue
@@ -5,7 +5,7 @@ export default {
     name: "page_connexion",
     data() {
         return {
-            loading : false,
+            loading: false,
             username: "",
             password: "",
             stay_connected: true,
@@ -17,7 +17,7 @@ export default {
             this.loading = true;
             await ClientAPI.connectAPI(this.username, this.password, this.stay_connected);
             if (ClientAPI.current_user) {
-                if (window.history.length > 1){
+                if (window.history.length > 1) {
                     router.go(-1);
                 } else {
                     router.replace('/');
@@ -36,26 +36,31 @@ export default {
             <form @submit.prevent="login" class="local d-flex flex-column my-5">
                 <label class="d-flex flex-column px-5 fw-bold fs-5">
                     Pseudo ou Adresse e-mail
-                    <input v-model="username" type="text" required class="my-2 p-2 border rounded"/>
+                    <input v-model="username" type="text" required class="my-2 p-2 border rounded" />
                 </label>
                 <div class="d-flex flex-grow-1 gap-5 my-3 py-3">
-                    <hr class="w-100 border-vert-neon border border-3"></hr>
-                    <hr class="w-25 border-vert-neon border border-3"></hr>
-                    <hr class="w-100 border-vert-neon border border-3"></hr>
+                    <hr class="w-100 border-vert-neon border border-3">
+                    </hr>
+                    <hr class="w-25 border-vert-neon border border-3">
+                    </hr>
+                    <hr class="w-100 border-vert-neon border border-3">
+                    </hr>
                 </div>
                 <label class="d-flex flex-column px-5 fw-bold fs-5">
                     Mot de passe
-                    <input v-model="password" type="password" required class="my-2 p-2 border rounded"/>
+                    <input v-model="password" type="password" required class="my-2 p-2 border rounded" />
                 </label>
                 <label class="d-flex justify-content-center px-5 fw-bold fs-5 gap-5">
                     Rester connecter ?
                     <!-- Refaire le style de la checkbox -->
-                    <input v-model="stay_connected" type="checkbox" class="my-2 p-2 border rounded"/>
+                    <div class="form-check form-switch">    
+                        <input v-model="stay_connected" type="checkbox" class="my-2 p-2 rounded form-check-input" />
+                    </div>
                 </label>
                 <div class="d-flex flex-column align-items-center m-5 gap-3">
                     <button class="p-2 w-25 h-100 border-0 rounded bg-vert-pale text-white">
                         <img v-if="this.loading" src="/imgs/spinner.gif" alt="loading image..." class="w-25">
-                        <span v-else="this.loading" >Valider</span>
+                        <span v-else="this.loading">Valider</span>
                     </button>
                     <!-- TODO -->
                     <a href="" class="p-2 text-white text-decoration-none fw-bold">Mot de passe oublié ?</a>
@@ -67,23 +72,26 @@ export default {
 
 <style scoped>
 .local label {
-    color : var(--blanc);
+    color: var(--blanc);
 }
+
 .border-vert-neon {
-    border-color : var(--vert-neon) !important;
+    border-color: var(--vert-neon) !important;
     opacity: 1;
 }
+
 .bg-vert-pale {
     background-color: var(--vert-pale);
 }
 
-input:-webkit-autofill{
-  -webkit-box-shadow: 0 0 0 1000px #fff inset !important; /* remplace #fff par ta couleur */
-  box-shadow: 0 0 0 1000px #fff inset !important;
-  background-clip: padding-box !important;
-  -webkit-text-fill-color: #000 !important;
-  color: #000 !important;
-  caret-color: #000 !important;
-  transition: background-color 5000s ease-in-out 0s !important;
+input:-webkit-autofill {
+    -webkit-box-shadow: 0 0 0 1000px #fff inset !important;
+    /* remplace #fff par ta couleur */
+    box-shadow: 0 0 0 1000px #fff inset !important;
+    background-clip: padding-box !important;
+    -webkit-text-fill-color: #000 !important;
+    color: #000 !important;
+    caret-color: #000 !important;
+    transition: background-color 5000s ease-in-out 0s !important;
 }
 </style>
