efcea6a33657552cc6623f4d4160190021ce3997
GangneuxPierre <pierre.gangneux@etu.univ-orleans.fr>
2026-01-23 08:14:12 +0100

    feature import csv - import de playlist via csv

--- backend/API/views/csv_import.py
+++ backend/API/views/csv_import.py
@@ -8,11 +8,13 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
-from ..models import Artiste, Audio, Question, Tag, Theme
+
+
+
+from ..models import Artiste, Audio, Occasion, Question, Tag, Theme
 
 from ..serializers import (
     ArtisteSerializer,
-    ThemeSerializer,
     QuestionSerializer,
     TagSerializer,
     ExtraitSerializer,
@@ -20,6 +22,8 @@ from ..serializers import (
     OccasionSerializer,
     AudiosSerializer,
     TagsExtraitRelationShipSerializer,
+    InterviewSerializer,
+    InterviewsSerializer,
 )
 
 
@@ -56,10 +60,25 @@ class CSVImportView(APIView):
 
         reader = csv.DictReader(io_string)
 
+
+        artiste = None
+        occasion = None
+        liste_extraits = []
         for row in reader:
-            print(row)  # Pour debug
-            print(type(row))  # Pour debug
-            self.save_row(row)
+            print(row)
+            if artiste != None:
+                if (artiste != row["Artiste"].strip() or occasion != row["Evenement"].strip()):
+                    print("iiiiiiiiiiiiiiiii")
+                    self.save_interview(liste_extraits, occasion)
+                    liste_extraits = []
+                    artiste = row["Artiste"].strip()
+                    occasion = row["Evenement"].strip()
+            else: 
+                artiste = row["Artiste"].strip()
+                occasion = row["Evenement"].strip()
+
+            extrait = self.save_row(row)
+            liste_extraits.append(extrait)
 
     def save_row(self, row):
         serializer_artiste = ArtisteSerializer()
@@ -150,10 +169,52 @@ class CSVImportView(APIView):
                 )
                 if serializer.is_valid():
                     serializer.create(serializer.validated_data)
+            
+            return extrait_node
 
         except Exception as e:
             print(f"Erreur lors de la création de l'extrait: {e}")
 
+    
+    def save_interview(self, liste_extraits, occasion):
+        if not liste_extraits:
+            return
+
+        serializer_interview = InterviewSerializer()
+        first_extrait = liste_extraits[0]
+        artiste_node = first_extrait.interviewer.single()
+
+        serializer_occasion = OccasionSerializer()
+        if occasion != "" and occasion is not None:
+            try:
+                occasion_node = serializer_occasion.create(
+                    {"name": occasion}
+                )
+            except:
+                occasion_node = Occasion.nodes.get(name=occasion)
+
+        titre = f"Interview de {artiste_node.name} pour {occasion_node.name if occasion_node else 'une occasion inconnue'}"
+        try:
+            interview_node = serializer_interview.create(
+                {
+                    "titre": titre,
+                    "date": first_extrait.uploaded_at,
+                    "description": "",
+                    "occasion_uuid": occasion_node.uuid if occasion_node else None,
+                }
+            )
+
+            for extrait in liste_extraits:
+                context = {"extrait": extrait}
+                serializer = InterviewsSerializer(
+                    data={"uuid": interview_node.uuid, "position":extrait.position}, context=context
+                )
+                if serializer.is_valid():
+                    serializer.create(serializer.validated_data)
+
+        except Exception as e:
+            print(f"Erreur lors de la création de l'interview: {e}")
+    
     def get_code(self, url):
         """
         Extrait le code vidéo d'une URL YouTube.
