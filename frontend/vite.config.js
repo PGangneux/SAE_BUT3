8d3f29ee9f27561095a80e4a9ed72a185341a64e
baptiste-mignan <baptiste.mignan@gmail.com>
2026-01-28 16:09:09 +0100

    feat(frontend): authentification frontend et refactor router

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@views': path.resolve(__dirname, 'src/views'),
+      '@model': path.resolve(__dirname, 'src/model'),
+      '@components': path.resolve(__dirname, 'src/components'),
+    }
+  },
   test: {
     globals: true,        // permet d'utiliser describe, it, expect directement
     environment: 'jsdom', // simule un navigateur
