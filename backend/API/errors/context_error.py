99d9f4349893da2bd1c90c414f379e8c31101844
baptiste-mignan <baptiste.mignan@gmail.com>
2026-02-07 16:29:30 +0100

    docs(api): documentation des erreurs

--- backend/API/errors/context_error.py
+++ backend/API/errors/context_error.py
@@ -6,10 +6,17 @@ class ContextError(ValidationError):
     """
     Erreur dans le context
     Node de context manquant
-    La vue demande un node dans le context mais n'en ressoit pas
+
+    Utiliser pour les vues qui ont besoin d'un node de context pour fonctionner (relationship viewset, sub base viewset)
     """
 
     def __init__(self, Class: StructuredNode = None):
+        """
+        Renvoie une erreur de validation avec un message d'erreur indiquant le node de context manquant
+
+        Args:
+            Class (StructuredNode, optional): Classe du node de context manquant. Defaults to None.
+        """
         super().__init__(
             {"Context error": Class.__name__ if Class is not None else None}
         )
